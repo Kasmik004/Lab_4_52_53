2bd1c713a225c49691d9f555f0c7c1db3b32eb3e
kritanrijal <gamerkritan@gmail.com>
2024-06-18 21:26:45 +0545

    Chore: Remove decls

--- src/sorting.h
+++ src/sorting.h
@@ -6,12 +6,4 @@ namespace sorting
     void insertion(int *, int, bool);
 }
 
-namespace measurement
-{
-    enum class MeasureType;
-
-    template <typename T>
-    long long time_since_epoch();
-}
-
 #endif
