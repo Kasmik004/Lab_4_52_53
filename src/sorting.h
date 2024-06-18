18a76df481a6a849ad4f36765150c42c82db8122
kritanrijal <gamerkritan@gmail.com>
2024-06-18 21:11:23 +0545

    Feat: Add `sorting.h`

---
+++ src/sorting.h
@@ -0,0 +1,17 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+namespace sorting
+{
+    void insertion(int *, int, bool);
+}
+
+namespace measurement
+{
+    enum class MeasureType;
+
+    template <typename T>
+    long long time_since_epoch();
+}
+
+#endif
